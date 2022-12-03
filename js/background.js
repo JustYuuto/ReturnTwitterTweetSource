fb6887aa8a73ba4500fcd956cb808f3006c5fa98
NetherMC <thelio.trinite@outlook.com>
2022-12-03 11:04:49 +0100

    first commit

---
+++ js/background.js
@@ -0,0 +1,58 @@
+if (!browser) var browser = chrome;
+
+browser.runtime.onMessage.addListener(async (message) => {
+    if (message.action === 'SET_SOURCE') {
+        const req = await fetch(`https://api.twitter.com/1.1/statuses/show/${message.data.id}.json`, {
+            headers: {
+                Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAGjvjwEAAAAA77GRcScylEupYSRdR229YWeba1Q%3DGgg1oobkcjxVsgcxNnLbmxikefkN1AX5LRToR0U3u9EqF1wB5c'
+            }
+        });
+        if (req.ok) {
+            const json = await req.json();
+            const parser = new DOMParser();
+            const sourceLink = parser.parseFromString(json.source, 'text/html');
+            const source = sourceLink.querySelector('a').innerText;
+            const interval = setInterval(() => {
+                const code = `
+                (() => {
+                    if (
+                        document.querySelector('.css-901oao.r-1bwzh9t.r-37j5jr.r-a023e6.r-16dba41.r-rjixqe.r-1b7u577.r-bcqeeo.r-qvutc0')?.childElementCount >= 2 &&
+                        !!document.querySelector('.css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2:last-of-type > .${message.data.el.replaceAll(' ', '.')}')
+                    ) {
+                        if (!browser) var browser = chrome;
+                        document.querySelector('.css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2:last-of-type > .${message.data.el.replaceAll(' ', '.')}').innerText = '${source.replaceAll("'", '&quot;')}';
+                        document.querySelector('.css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2:last-of-type > .${message.data.el.replaceAll(' ', '.')}').setAttribute('aria-label', '${source.replaceAll("'", '&quot;')}');
+                        document.querySelector('.css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2:last-of-type > .${message.data.el.replaceAll(' ', '.')}').setAttribute('title', '${source.replaceAll("'", '&quot;')}');
+                        document.querySelector('.css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2:last-of-type > .${message.data.el.replaceAll(' ', '.')}').setAttribute('role', 'link');
+                        return true;
+                    } else { return false; }
+                })();
+                `;
+                browser.tabs.executeScript({ code }, (result) => {
+                    return !!result[0] && clearInterval(interval);
+                });
+            }, 500);
+        } else {
+            const interval = setInterval(() => {
+                const code = `
+                (() => {
+                    if (
+                        document.querySelector('.css-901oao.r-1bwzh9t.r-37j5jr.r-a023e6.r-16dba41.r-rjixqe.r-1b7u577.r-bcqeeo.r-qvutc0')?.childElementCount >= 2 &&
+                        !!document.querySelector('.css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2:last-of-type > .${message.data.el.replaceAll(' ', '.')}')
+                    ) {
+                        if (!browser) var browser = chrome;
+                        document.querySelector('.css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2:last-of-type > .${message.data.el.replaceAll(' ', '.')}').innerHTML = '<span style="color: red;">An error occured while loading source (click for more info)</span>';
+                        document.querySelector('.css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2:last-of-type > .${message.data.el.replaceAll(' ', '.')}').setAttribute('aria-label', 'Cannot get protected tweets and Twitter circle tweets status due to the API');
+                        document.querySelector('.css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2:last-of-type > .${message.data.el.replaceAll(' ', '.')}').setAttribute('title', 'Cannot get protected tweets and Twitter circle tweets status due to the API');
+                        document.querySelector('.css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2:last-of-type > .${message.data.el.replaceAll(' ', '.')}').setAttribute('role', 'link');
+                        return true;
+                    } else { return false; }
+                })();
+                `;
+                browser.tabs.executeScript({ code }, (result) => {
+                    return !!result[0] && clearInterval(interval);
+                });
+            }, 500);
+        }
+    }
+});
---
+++ js/content-script.js
@@ -0,0 +1,62 @@
+if (!browser) var browser = chrome;
+
+const div = document.createElement('div');
+const link = document.createElement('a');
+const dot = document.createElement('span');
+const tweetPathRegex = /\/([a-zA-Z0-9_]+)\/status\/([0-9]+)/gi;
+
+const createHtml = () => {
+    div.classList.add('css-1dbjc4n', 'r-1d09ksm', 'r-1471scf', 'r-18u37iz', 'r-1wbh5a2');
+    dot.innerText = ' · ';
+
+    link.classList.add(
+        'css-4rbku5', 'css-18t94o4', 'css-901oao', 'css-16my406', 'r-1bwzh9t', 'r-1loqt21', 'r-xoduu5', 'r-1q142lx',
+        'r-1w6e6rj', 'r-poiln3', 'r-9aw3ui', 'r-bcqeeo', 'r-3s2u2q', 'r-qvutc0'
+    );
+    link.innerText = 'Loading...';
+    link.setAttribute('href', 'https://help.twitter.com/using-twitter/how-to-tweet#source-labels');
+    link.setAttribute('target', '_blank');
+
+    link.addEventListener('mouseenter', () => {
+        link.className =
+            'css-4rbku5 css-18t94o4 css-901oao css-16my406 r-1bwzh9t r-1loqt21 r-xoduu5 r-1q142lx r-1w6e6rj r-poiln3 r-9aw3ui r-bcqeeo r-1ny4l3l r-1ddef8g r-tjvw6i r-3s2u2q r-qvutc0';
+    });
+    link.addEventListener('mouseleave', () => {
+        link.className =
+            'css-4rbku5 css-18t94o4 css-901oao css-16my406 r-1bwzh9t r-1loqt21 r-xoduu5 r-1q142lx r-1w6e6rj r-poiln3 r-9aw3ui r-bcqeeo r-3s2u2q r-qvutc0';
+    });
+
+    div.appendChild(dot);
+    div.appendChild(link);
+}
+
+const fetchSource = async (link) => {
+    const tweetId = window.location.pathname.split('/')[3];
+    await browser.runtime.sendMessage({ action: 'SET_SOURCE', data: { id: tweetId, el: link.className } });
+}
+
+const apply = () => {
+    const interval = setInterval(() => {
+        const el = document.querySelector('.css-901oao.r-1bwzh9t.r-37j5jr.r-a023e6.r-16dba41.r-rjixqe.r-1b7u577.r-bcqeeo.r-qvutc0');
+        if (!!el) {
+            el.appendChild(div);
+            clearInterval(interval);
+        }
+    }, 500);
+    fetchSource(link);
+}
+
+let currentPath = window.location.pathname;
+setInterval(() => {
+    if (typeof window.location.pathname.match(tweetPathRegex)[0] === 'string') {
+        if (!document.querySelector('.css-1dbjc4n.r-1d09ksm.r-1471scf.r-18u37iz.r-1wbh5a2:nth-child(2)')) {
+            createHtml();
+            apply();
+        }
+    }
+}, 500);
+
+if (!!window.location.pathname.match(tweetPathRegex)) {
+    createHtml();
+    apply();
+}
